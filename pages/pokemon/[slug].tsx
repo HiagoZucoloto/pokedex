5851fee2c49c10381d63daed968ec3efe4093309
HiagoZucoloto <cristofferin@gmail.com>
2023-08-17 19:03:11 -0300

    swiper in routes

--- pages/pokemon/[slug].tsx
+++ pages/pokemon/[slug].tsx
@@ -1,6 +1,20 @@
 import { GetServerSideProps } from "next";
 import Link from "next/link";
 
+import React, { useRef, useState } from "react";
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import "swiper/css/pagination";
+
+const swiperBar = [
+  {
+    form: "Forms",
+    detail: "Details",
+    type: "Types",
+    stat: "Stats",
+  },
+];
+
 interface PokemonDetail {
   name: string;
   image: string;
@@ -64,9 +78,73 @@ const PokemonPage: React.FC<PokemonProps> = ({ pokemon }) => {
         </div>
       </section>
 
-      <section className="py-4">SWIPER INFO**</section>
+      <section className="py-4">
+        {swiperBar.map((item, key) => (
+          <Swiper
+            key={key}
+            pagination={{
+              clickable: true,
+            }}
+            breakpoints={{
+              460: {
+                slidesPerView: 3,
+                spaceBetween: 4,
+              },
+              650: {
+                slidesPerView: 4,
+                spaceBetween: 8,
+              },
+              768: {
+                slidesPerView: 5,
+                spaceBetween: 16,
+              },
+            }}
+            className="mySwiper pr-40"
+          >
+            <SwiperSlide>
+              <div className="text-lg font-semibold">{item.form}</div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="text-lg font-semibold">{item.detail}</div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="text-lg font-semibold">{item.type}</div>
+            </SwiperSlide>
+            <SwiperSlide>
+              <div className="text-lg font-semibold">{item.stat}</div>
+            </SwiperSlide>
+          </Swiper>
+        ))}
+      </section>
 
-      <section>MEGA EVO IMG**</section>
+      <section>
+        <div className="flex gap-4">
+          <div className="bg-gray-200 rounded-2xl">
+            <img
+              src={pokemon.image}
+              alt={pokemon.name}
+              width={"100px"}
+              height={"120px"}
+            />
+          </div>
+          <div className="bg-gray-200 rounded-2xl">
+            <img
+              src={pokemon.image}
+              alt={pokemon.name}
+              width={"100px"}
+              height={"120px"}
+            />
+          </div>
+          <div className="bg-gray-200 rounded-2xl">
+            <img
+              src={pokemon.image}
+              alt={pokemon.name}
+              width={"100px"}
+              height={"120px"}
+            />
+          </div>
+        </div>
+      </section>
 
       <section className="py-4 grid gap-1">
         <div className="text-md font-medium">Mega Evolution</div>
