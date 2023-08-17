6c8ee4fbf349741d2e89ad209e31ba87e49a9267
HiagoZucoloto <cristofferin@gmail.com>
2023-08-17 15:31:21 -0300

    *

--- pages/pokemon/[id].tsx
+++ pages/pokemon/[id].tsx
@@ -1,4 +1,5 @@
 import { GetServerSideProps } from "next";
+import Link from "next/link";
 
 interface PokemonDetail {
   name: string;
@@ -39,11 +40,39 @@ export const getServerSideProps: GetServerSideProps = async ({ params }) => {
 
 const PokemonPage: React.FC<PokemonProps> = ({ pokemon }) => {
   return (
-    <div>
-      <h1>{pokemon.name}</h1>
-      <img src={pokemon.image} alt={pokemon.name} />
-      {/* outras informações dos pokemons  */}
-    </div>
+    <main className="px-8">
+      <header className="flex w-full pt-12">
+        <Link href={"/"} className="w-[10%]">
+          {"<"}
+        </Link>
+
+        <div className="w-[90%] grid gap-1 text-center">
+          <div className="text-3xl">{pokemon.name}</div>
+          <div>{pokemon.id}</div>
+        </div>
+      </header>
+
+      <section className="bg-gray-200 rounded-xl mt-6">
+        <div className="text-center py-2">
+          <img
+            src={pokemon.image}
+            alt={pokemon.name}
+            className="mx-auto"
+            width={"200px"}
+            height={"200px"}
+          />
+        </div>
+      </section>
+
+      <section className="py-4">SWIPER INFO**</section>
+
+      <section>MEGA EVO IMG**</section>
+
+      <section className="py-4 grid gap-1">
+        <div className="text-md font-medium">Mega Evolution</div>
+        <div className="text-md">Info of pokemon text</div>
+      </section>
+    </main>
   );
 };
 
